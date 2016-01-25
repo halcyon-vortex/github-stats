f77bd318005b158ac84891edeafd4ae24a98b5b3
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-25 12:19:05 -0800

    work on parsing pattern

---
+++ src/parse-sync.js
@@ -0,0 +1,47 @@
+var fs = require('fs');
+var _ = require('lodash');
+
+var file = JSON.parse(fs.readFileSync('user_get_allrepos_100.txt'));
+
+var result = _.reduce(file, (acc, value, i, array) => {
+  if (!value.fork) {
+    let {
+      name,
+      full_name,
+      description,
+      created_at,
+      updated_at,
+      pushed_at,
+      size,
+      stargazers_count,
+      watchers_count,
+      language,
+      forks_count,
+      forks,
+      watchers,
+      has_issues,
+      open_issues,
+      open_issues_count
+    } = value;
+
+      acc.push({
+        name,
+        full_name,
+        created_at,
+        updated_at,
+        pushed_at,
+        size,
+        stargazers_count,
+        watchers_count,
+        language,
+        forks_count,
+        forks,
+        watchers,
+        open_issues,
+        open_issues_count
+      });
+  }
+  return acc;
+}, []);
+console.log(result.length);
+//console.log(result);

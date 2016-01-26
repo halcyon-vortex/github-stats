a628cf4d1f67f1a0fd75f059bd6f2d937f597c06
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-25 19:44:19 -0800

    get ready to add logging

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import Client from 'github';
 import fs from 'fs';
 import parseRepos from './parse-repos.js';
+import parseStargazers from './parse-stargazers.js'
 let github = new Client({
   debug: true,
   version: "3.0.0"
@@ -29,15 +30,13 @@ github.authenticate({
 github.repos.getFromUser({user: 'tj', per_page: 100}, (err, res) => {
   let repos = parseRepos(res);
   fs.writeFile("tj_repos_data.json", JSON.stringify(repos, null, 4), (err, res) => console.log('wrote repos data'));
-  let moreThanTenStars = _.reduce(repos, (acc, repo, i, arr) => {
-    if (repo.stargazers_count > 10) {
+  let moreThanFiveStars = _.reduce(repos, (acc, repo, i, arr) => {
+    if (repo.stargazers_count > 5) {
       return acc.concat(repo.name);
     }
     return acc;
   }, []);
-  console.log('repos with more than 10 stars');
-  console.log(moreThanTenStars);
-  _.forEach(moreThanTenStars, (repo, index) => {
+  _.forEach(moreThanFiveStars, (repo, index) => {
     // don't want to slam github with too many api requests per second
     setTimeout(() => {
       console.log('initializing request for: ' + repo);

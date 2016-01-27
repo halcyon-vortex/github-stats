ee563b919ca333e3dedea66a703116b7c29750cc
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-27 15:49:56 -0800

    better implementation so can just node src/server.js and everything else can be written in es6

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-require("babel-core/register");
-require("babel-polyfill");
-var Client = require('github');
-var processUser = require('./process-user-aa');
-var github = new Client({
+import Client from 'github';
+import processUser from './process-user-aa';
+let github = new Client({
   debug: true,
   version: "3.0.0"
 });

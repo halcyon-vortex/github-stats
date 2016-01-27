cceee327c4370c7e93e4675d6421a1a6a56577d4
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-26 21:41:46 -0800

    request with authorization as the node-github client doesnt allow to look up user stats

--- src/process-user-aa.js
+++ src/process-user-aa.js
@@ -2,19 +2,24 @@ import _ from 'lodash';
 import parseRepos from './parse-repos.js';
 import Promise from 'bluebird';
 import winston from 'winston';
-let fs = Promise.promisify(require('fs'));
+let fs = Promise.promisifyAll(require('fs'));
+let request = Promise.promisifyAll(require('superagent'));
 winston.add(winston.transports.File, { filename: '../download_status.log' });
 winston.remove(winston.transports.Console);
 
 let processUser = async function(authenticatedGithubClient, user) {
   // promisified methods
-  
+
+  let getUser = Promise.promisify(authenticatedGithubClient.user.get,
+    {context: authenticatedGithubClient});
   let getReposFromUser = Promise.promisify(authenticatedGithubClient.repos.getFromUser,
     {context: authenticatedGithubClient});
 
   let getStargazersFromRepo = Promise.promisify(authenticatedGithubClient.repos.getStargazers,
     {context: authenticatedGithubClient});
 
+  let getLanguagesFromRepo = Promise.promisify(authenticatedGithubClient.repos.getLanguages,
+    {context: authenticatedGithubClient});
   // get user information (key --> number of repos for pagination purposes)
 
   // get repos, paginating as necessary
@@ -24,7 +29,15 @@ let processUser = async function(authenticatedGithubClient, user) {
   // store everything
 
   console.log('searching');
-  let raw_results = await getReposFromUser({user: "tj", per_page: 10});
-  console.log(raw_results)
+  //let userInfo = await request.getAsync('https://api.github.com/users/tj');
+  let rate_limit = await request.getAsync(`https://dpastoor:${process.env.GHPW}@api.github.com/rate_limit`);
+
+  console.log('------- rate limit -------');
+  console.log(rate_limit);
+  //let body = userInfo.body;
+  //let isOrg = body.type === 'User';
+  //let numRepos = body.public_repos;
+  //let raw_results = await getReposFromUser({user: "tj", per_page: 10});
+  //console.log(raw_results)
 };
 export default processUser;

0648b01b3c60153d62254a57c5915aeb4e8f681f
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-26 20:24:23 -0800

    add pseudocode for processing-user and start promisifying necessary methods

--- src/process-user-aa.js
+++ src/process-user-aa.js
@@ -1,15 +1,28 @@
 import _ from 'lodash';
-import fs from 'fs';
 import parseRepos from './parse-repos.js';
 import Promise from 'bluebird';
 import winston from 'winston';
-
+let fs = Promise.promisify(require('fs'));
 winston.add(winston.transports.File, { filename: '../download_status.log' });
 winston.remove(winston.transports.Console);
 
 let processUser = async function(authenticatedGithubClient, user) {
+  // promisified methods
+  
   let getReposFromUser = Promise.promisify(authenticatedGithubClient.repos.getFromUser,
     {context: authenticatedGithubClient});
+
+  let getStargazersFromRepo = Promise.promisify(authenticatedGithubClient.repos.getStargazers,
+    {context: authenticatedGithubClient});
+
+  // get user information (key --> number of repos for pagination purposes)
+
+  // get repos, paginating as necessary
+
+  // for each repo get key information + stargazers
+
+  // store everything
+
   console.log('searching');
   let raw_results = await getReposFromUser({user: "tj", per_page: 10});
   console.log(raw_results)

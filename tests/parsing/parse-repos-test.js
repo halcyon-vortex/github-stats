76603094fd3faf8010e333bb1c8ac5fb4145f631
Devin Pastoor <devin.pastoor@gmail.com>
2016-02-03 11:52:44 -0800

    remove unecessary writeFile

--- tests/parsing/parse-repos-test.js
+++ tests/parsing/parse-repos-test.js
@@ -6,13 +6,11 @@ import test from 'tape';
 import parseRepos from '../../src/parsing/parse-repos';
 import mockData from './mockData/mockRepos';
 import mockDataParsed from './mockData/mockReposParsed';
-import fs from 'fs';
 test('Assertions with tape.', (assert) => {
   const expected =  mockDataParsed.parsedRepos;
   const expectedStarred =  mockDataParsed.parsedReposStarredAt;
   const actual = parseRepos(mockData.Repos);
   const actualStarred = parseRepos(mockData.ReposStarredAt);
-  fs.writeFileSync('jsonoutput.json', JSON.stringify(actualStarred, null, 4));
   assert.deepEqual(actual, expected,
     'All user repos appropriately parsed');
   assert.deepEqual(actualStarred, expectedStarred,

78381d64d65b83a4a8ebea4da5e6f4efda7f1f4e
Devin Pastoor <devin.pastoor@gmail.com>
2016-02-03 11:31:32 -0800

    refactor parse-repos to handle starred_at

--- src/parsing/parse-repos.js
+++ src/parsing/parse-repos.js
@@ -1,46 +1,56 @@
 import _ from 'lodash';
 
 export default (file) => _.reduce(file, (acc, value, i, array) => {
-  if (!value.fork) {
-    let {
-      name,
-      full_name,
-      description,
-      created_at,
-      updated_at,
-      pushed_at,
-      size,
-      stargazers_count,
-      watchers_count,
-      language,
-      forks_count,
-      forks,
-      watchers,
-      has_issues,
-      open_issues,
-      open_issues_count,
-      owner,
-      fork
-    } = value;
-
-      acc.push({
-        name,
-        full_name,
-        created_at,
-        updated_at,
-        pushed_at,
-        size,
-        stargazers_count,
-        watchers_count,
-        language,
-        forks_count,
-        forks,
-        watchers,
-        open_issues,
-        open_issues_count,
-        owner: owner.login,
-        fork
-      });
+  let starred_at = null;
+  if (value.hasOwnProperty('starred_at')) {
+    // if passing in headers for starred at, the original json object is all pushed into the repo property
+    starred_at = value.starred_at;
+    value = value.repo;
   }
-  return acc;
+
+  let {
+    name,
+    full_name,
+    description,
+    created_at,
+    updated_at,
+    pushed_at,
+    size,
+    stargazers_count,
+    watchers_count,
+    language,
+    forks_count,
+    forks,
+    watchers,
+    has_issues,
+    open_issues,
+    open_issues_count,
+    owner,
+    fork
+  } = value;
+
+  let output = {
+    name,
+    full_name,
+    created_at,
+    updated_at,
+    pushed_at,
+    size,
+    stargazers_count,
+    watchers_count,
+    language,
+    forks_count,
+    forks,
+    watchers,
+    open_issues,
+    open_issues_count,
+    owner: owner.login,
+    fork
+  };
+
+  if (starred_at) {
+    output["starred_at"] = starred_at;
+  }
+
+  return acc.concat(output);
 }, []);
